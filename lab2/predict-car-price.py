593493966497e88587b0b117cc08e28746e25f9b
Saketh Reddy <sakethreddy.333@gmail.com>
2021-03-09 18:41:24 -0800

    lab2 initial changes

--- lab2/predict-car-price.py
+++ lab2/predict-car-price.py
@@ -17,6 +17,21 @@ class CarPrice:
             self.df[col] = self.df[col].str.lower().str.replace(' ', '_')
 
     def validate(self):
+        np.random.seed(2)
+        n = len(df)
+
+        n_val = int(0.2 * n)
+        n_test = int(0.2 * n)
+        n_train = n - (n_val + n_test)
+
+        idx = np.arange(n)
+        np.random.shuffle(idx)
+
+        df_shuffled = df.iloc[idx]
+
+        df_train = df_shuffled.iloc[:n_train].copy()
+        df_val = df_shuffled.iloc[n_train:n_train+n_val].copy()
+        df_test = df_shuffled.iloc[n_train+n_val:].copy()
         pass
 
     def linear_regression(self, X, y):
